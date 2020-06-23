24a0a47d6faa5b6ee942fc8904c075d7e6050db9
Fuad Al Amin <fuad01723102003@gmail.com>
2020-06-23 17:31:43 +0600

    update best_answer_id on questions table when answer removed in database layer

--- app/Answer.php
+++ app/Answer.php
@@ -28,11 +28,15 @@ class Answer extends Model
         });
 
         static::deleted(function($answer) {
-            $answer->question->decrement('answers_count');
+            $answer->question->decrement('answers_count'); 
         });
     }
     
     public function getCreatedDateAttribute(){
         return $this->created_at->diffForHumans();
+    }
+
+    public function getStatusAttribute(){
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
     }
 }
